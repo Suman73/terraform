7c5c089be6fe60941f26f32353f25c4b5058caed
Suman Pramanik <40729668+Suman73@users.noreply.github.com>
2023-08-03 18:08:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,6 @@
 # main.tf
 
-terraform {
-  required_version = ">= 0.13"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"  # Replace with the desired version constraint for the AWS provider
-    }
-  }
-}
 
 provider "aws" {
   region = var.region
